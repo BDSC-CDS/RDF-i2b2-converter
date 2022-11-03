3c41915ea6e5c98877073994317a9bb2aebb5379
Jules Fasquelle <fasjul@gmail.com>
2022-11-03 17:40:06 +0100

    dimensions are populated correctly and from an external directory

--- src/scripts/postprod.bash
+++ src/scripts/postprod.bash
@@ -5,23 +5,20 @@ PROJECT="BIOREF"
 PROD_FOLDER="$(pwd)/output_tables/"
 DEBUG_FOLDER="$(pwd)/debug_tables/"
 
-
 main () {
     reindex
     fill_default_values
-
-    if [ $1!="--skip-replacing" ] ;then
+    if ! [ $1=="--skip-replacing" ] ;then
         replace_codes $PROD_FOLDER $DEBUG_FOLDER
     fi
-
 }
-
 
 fill_default_values() {
     awk -v defdate=$DEFAULT_DATE '($4==""){$4="@"}($5==""){$5=defdate}1' FS=, OFS=, OBSERVATION_FACT.csv > tmp && mv -f tmp OBSERVATION_FACT.csv
 }
 
 replace_codes () {
+    echo "Replacing debug codes by production codes"
     PATH_HCD=$1/CONCEPT_DIMENSION.csv
     PATH_HMD=$1/MODIFIER_DIMENSION.csv
     PATH_HOF=$1/OBSERVATION_FACT.csv
@@ -44,61 +41,63 @@ replace_codes () {
 
     # 2. replace column of OBSERVATION fact by their equivalent in non-debug mode
     awk 'FNR==NR{a[$2]=$1;next}{ $3=a[$3]}1' FS=, OFS=, $LOOKUP_CONCEPTS $PATH_DOF > $1/tmp
-
     awk 'FNR==NR{a[$2]=$1;next}{ $6=a[$6]}1' FS=, OFS=, $LOOKUP_MODIFIERS $1/tmp > $PATH_HOF && rm -f $1/tmp
+    echo "Debug codes were correctly replaced."
 }
 
 reindex () {
-
     # This function takes care of patient_dimension reindexing and filling of patient_mapping accordingly. 
     # It also does the equivalent job for visit_dimension and encounter_mapping
     # Required: filled patient_dimension and visit_dimension, header-only encounter_mapping and patient_mapping.
-
-
     # Source on OBSERVATION_FACT and extract a pruned list of (encounter,patient) pairs with it
-
-    awk '(NR>1){print $2 FS $1}' FS=, OFS=, OBSERVATION_FACT.csv | awk '!visited[$0]++' FS=, OFS=, > unique_PE_pairs.csv
+    echo "Starting reindexing"
+    awk '(NR>1){print $1 FS $2}' FS=, OFS=, OBSERVATION_FACT.csv | awk '!visited[$0]++' FS=, OFS=, > unique_PE_pairs.csv
     reindex_encounters
     reindex_patients
-
     rm -f unique_PE_pairs.csv 
 }
 
 reindex_encounters () {
+    echo "Reindexing encounters..."
     awk -v proj=$PROJECT -v default="-1" '(NR==1){print $0; cnter=1; next}
-            (FNR>1 && $1!="" && !visited[$1]++){print $1 FS $1 FS proj FS cnter++ FS -1 FS -1 FS FS FS FS FS FS FS}' \
+            (NR>FNR && $1!="" && !visited[$1]++){print $1 FS $1 FS proj FS cnter++ FS default FS default FS FS FS FS FS FS FS}' \
         FS=, OFS=, ENCOUNTER_MAPPING.csv unique_PE_pairs.csv > tmp \
-        && mv tmp -f ENCOUNTER_MAPPING.csv
+        && mv -f tmp ENCOUNTER_MAPPING.csv
+    echo "ENCOUNTER_MAPPING written."
 
     # Fill VISIT_DIMENSION accordingly
     awk '(NR==1){print $0}(NR!=FNR && FNR>1){print $4 FS $4 FS FS FS FS FS FS FS FS FS FS FS FS FS}' \
         FS=, OFS=, VISIT_DIMENSION.csv ENCOUNTER_MAPPING.csv > tmp \
         && mv -f tmp VISIT_DIMENSION.csv
+    echo "VISIT_DIMENSION written."
 
     # Read ENCOUNTER_MAPPING and replace values in OBSERVATION_FACT
-    awk '(FNR==NR){a[$1]=$4;next}(FNR==1){print $0;next}{$2=a[$2]; print $0}' FS=, OFS=, ENCOUNTER_MAPPING.csv OBSERVATION_FACT.csv > tmp \\
+    awk '(FNR==NR){a[$1]=$4;next}(FNR!=1){$1=a[$1]}{print $0}' \
+        FS=, OFS=, ENCOUNTER_MAPPING.csv OBSERVATION_FACT.csv > tmp \
         && mv -f tmp OBSERVATION_FACT.csv
+    echo "Reindexed encounters in OBSERVATION_FACT."
 
 }
 
 reindex_patients () {
-    ################
-    # Reindex patients and fill patient_mapping + patient_dimension
-    ################
-
     # Fill PATIENT_MAPPING with the info and index from PATIENT_DIMENSION
-    awk -v proj=$PROJECT '(NR==1){print $0; next}(FNR>1 && !visited[$2]++){print $2 FS $2 FS FNR-1 FS FS proj FS FS FS FS FS FS}' \
-        FS=, OFS=, PATIENT_MAPPING.csv unique_PE_pairs.csv > tmp \\
-        && mv tmp -f PATIENT_MAPPING.csv
+    awk -v proj=$PROJECT '(NR==1){print $0; cnter=1; next} \
+        (NR>FNR && !visited[$2]++){print $2 FS $2 FS cnter++ FS FS proj FS FS FS FS FS FS}' \
+        FS=, OFS=, PATIENT_MAPPING.csv unique_PE_pairs.csv > tmp \
+        && mv -f tmp PATIENT_MAPPING.csv
+    echo "PATIENT_MAPPING written."
 
     # Fill PATIENT_DIMENSION accordingly
     awk '(NR==1){print $0}(NR!=FNR && FNR>1){print $3 FS $3 FS FS FS FS FS FS FS FS FS FS FS FS FS}' \
         FS=, OFS=, PATIENT_DIMENSION.csv PATIENT_MAPPING.csv > tmp \
         && mv -f tmp PATIENT_DIMENSION.csv
+    echo "PATIENT_DIMENSION written."
 
     # Read PATIENT_MAPPING and replace values in OBSERVATION_FACT
-    awk '(FNR==NR){a[$1]=$3;next}(FNR!=1){$2=a[$2]}{print $0}' FS=, OFS=, PATIENT_MAPPING.csv OBSERVATION_FACT.csv > tmp && mv tmp -f OBSERVATION_FACT.csv
-
+    awk '(FNR==NR){a[$1]=$3;next}(FNR!=1){$2=a[$2]}{print $0}' \
+        FS=, OFS=, PATIENT_MAPPING.csv OBSERVATION_FACT.csv > tmp \
+        && mv -f tmp OBSERVATION_FACT.csv
+    echo "Reindexed patients in OBSERVATION_FACT."
 }
 
 main $@
